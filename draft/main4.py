8be96c2329d15d228e9b268180fe853d49f8887e
Erwan Moreau <moreaue@cs.tcd.ie>
2023-12-29 16:18:43 +0000

    nothing works

--- draft/main4.py
+++ draft/main4.py
@@ -58,28 +58,7 @@ def apply_sentiment_model(sentences):
 
 
 
-def custom_loss(logits, labels, target_sentiment):
-
-    # Apply softmax to obtain probabilities
-    probabilities = torch.nn.functional.softmax(logits, dim=-1)
-    # Sample from the distribution to get predicted tokens
-    predicted_token_ids = torch.multinomial(probabilities[:, -1, :], 1)
-    print(logits.shape, probabilities.shape, predicted_token_ids.shape)
-    # Decode the predicted token IDs to obtain the actual tokens                       
-    tokens = [tokenizer.decode(sent_token_ids.squeeze().item(), skip_special_tokens=True) for sent_token_ids in predicted_token_ids]
-    for tokens0 in tokens:
-        print(" ".join(tokens0)) 
-    # Compute the sentiment of the generated sentences
-    sentiment_outputs = apply_sentiment_model(tokens)
-    a=[]
-    for senti in sentiment_outputs:
-        for k,score in senti.items():
-            a.append(abs(target_sentiment.get(k) - score)) 
-    loss = torch.tensor(a, requires_grad=True).sum()
-    return loss
-
-
-def custom_loss2(generated_sentences, target_sentiment):
+def custom_loss(generated_sentences, target_sentiment):
 
     # Compute the sentiment of the generated sentences
     sentiment_outputs = apply_sentiment_model(generated_sentences)
@@ -87,6 +66,7 @@ def custom_loss2(generated_sentences, target_sentiment):
     for senti in sentiment_outputs:
         for k,score in senti.items():
             a.append(abs(target_sentiment.get(k) - score)) 
+    print(a)
     loss = torch.tensor(a, requires_grad=True).sum()
     return loss
 
@@ -101,7 +81,8 @@ def fine_tune(training_data, sentiment_target, num_epochs, lr, batch_size, num_t
     dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=False)
 
     # Set up optimizer
-    optimizer = optim.AdamW(model.parameters(), lr=lr)
+    #optimizer = optim.AdamW(model.parameters(), lr=lr)
+    optimizer = optim.SGD(model.parameters(), lr=lr)
 
     # Fine-tuning loop
     for epoch in range(num_epochs):
@@ -124,21 +105,14 @@ def fine_tune(training_data, sentiment_target, num_epochs, lr, batch_size, num_t
                 input_ids = torch.cat([input_ids, next_token], dim=-1)
                 new_tokens = torch.cat([new_tokens, next_token], dim=-1)
 
-            #print(new_tokens.shape)
-            generated_sentences=[]
-            for seq in new_tokens:
-                seq[seq >= tokenizer.vocab_size] = tokenizer.unk_token_id
-                print(seq)
-                generated_sentences.append(tokenizer.decode(seq.tolist(), skip_special_tokens=True))
-#            print(" ".join([str(l) for l in [ len(seq) for seq in new_tokens]]))
-#            generated_sentences = [tokenizer.decode(seq.tolist()) for seq in new_tokens]
-            #for tokens in generated_sentences:
-            #    print(tokens) 
+                generated_sentences=[]
+                for seq in new_tokens:
+                    seq[seq >= tokenizer.vocab_size] = tokenizer.unk_token_id
+                    generated_sentences.append(tokenizer.decode(seq.tolist(), skip_special_tokens=True))
+                #for tokens in generated_sentences:
+                #    print(tokens) 
 
-#            outputs = model(input_ids, attention_mask=attention_mask, labels=labels)
-#            loss = custom_loss(outputs.logits, labels,  sentiment_target)
-            loss = custom_loss2(generated_sentences, sentiment_target)
-
+            loss = custom_loss(generated_sentences, sentiment_target)
             loss.backward()
             optimizer.step()
 
@@ -166,10 +140,11 @@ def mymain():
 
     target_sentiment0 = apply_sentiment_model([args.input_string])
     target_sentiment = target_sentiment0[0]
+    print("Target sentiment: ", target_sentiment)
 
     data = generate_some_sentences(model, tokenizer, args.input_string, args.sample_size)
     for i,t in enumerate(data):
-        print(f"{i}: {t}")
+        print(f"{i+1}: {t}")
 
     fine_tune(data, target_sentiment, args.num_epochs, args.learn_rate, args.batch_size)
 
